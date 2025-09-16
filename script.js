65685faa8e6a3bd48301a34c54c0473e03d98755
REISINSO2024 <motoh5018@gmail.com>
2025-09-15 22:51:15 -0300

    Update script.js
    CORREÇÃO

--- script.js
+++ script.js
@@ -375,10 +375,19 @@ function atualizarProgramados() {
     const caes = dadosQuadrasSelecionadas.reduce((acc, cur) => acc + getNumero(cur.CÃO || 0), 0);
     const gatos = dadosQuadrasSelecionadas.reduce((acc, cur) => acc + getNumero(cur.GATO || 0), 0);
     
-    // Depósitos de água
-    const depositos = dadosQuadrasSelecionadas.reduce((acc, cur) => {
-        return acc + getNumero(cur['TANQUE EXISTENTE']) + getNumero(cur['TAMBOR EXISTENTE']) + getNumero(cur['CISTERNA EXISTENTE']) + getNumero(cur['CACIMBA EXISTENTE']) + getNumero(cur["CAIXAS D'ÁGUA EXISTENTE"])getNumero(cur['FILTRO'])getNumero(cur["VASO C/PLANTA"])getNumero(cur['TINA']);
-    }, 0);
+   // Depósitos de água
+const depositos = dadosQuadrasSelecionadas.reduce((acc, cur) => {
+    return acc
+        + getNumero(cur['TANQUE EXISTENTE'])
+        + getNumero(cur['TAMBOR EXISTENTE'])
+        + getNumero(cur['CISTERNA EXISTENTE'])
+        + getNumero(cur['CACIMBA EXISTENTE'])
+        + getNumero(cur["CAIXAS D'ÁGUA EXISTENTE"])
+        + getNumero(cur['FILTRO'])
+        + getNumero(cur["VASO C/PLANTA"])
+        + getNumero(cur['TINA']);
+}, 0);
+
 
     const imoveisProgramados = totalImoveis - apartamentos;
 
@@ -941,6 +950,7 @@ document.addEventListener("DOMContentLoaded", function() {
     
     console.log("Sistema inicializado com sucesso!");
 });
+
 
 
 
